1709ce28f7437a84c05a8b85b2d74730fefb9d8c
Kilian Boute <kilian.boute@gmail.com>
2024-01-05 15:45:52 +0100

    Add validation to form

--- form-view.php
+++ form-view.php
@@ -1,85 +1,88 @@
-<?php // This file is mostly containing things for your view / html ?>
+<?php // This file is mostly containing things for your view / html 
+?>
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
-          rel="stylesheet"/>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
     <title>Your fancy store</title>
 </head>
+
 <body>
-<div class="container">
-    <h1>Place your order</h1>
-    <?php // Navigation for when you need it ?> 
-    <nav>
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="?food=1">Order cereal</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?food=0">Order drinks</a>
-            </li>
-        </ul>
-    </nav>
-    
-    <form method="POST">
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="email">E-mail:</label>
-                <input type="email" id="email" name="email" class="form-control"/>
-            </div>
-            <div></div>
-        </div>
+    <div class="container">
+        <h1>Place your order</h1>
+        <?php // Navigation for when you need it 
+        ?>
+        <nav>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="?food=1">Order cereal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?food=0">Order drinks</a>
+                </li>
+            </ul>
+        </nav>
 
-        <fieldset>
-            <legend>Address</legend>
-
+        <form method="POST">
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control">
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" name="email" class="form-control" value="<?= isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '' ?>" />
                 </div>
-                <div class="form-group col-md-6">
-                    <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control">
-                </div>
+                <div></div>
             </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control">
+
+            <fieldset>
+                <legend>Address</legend>
+
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="street">Street:</label>
+                        <input type="text" name="street" id="street" class="form-control" value="<?= isset($_POST['street']) ? htmlspecialchars($_POST['street']) : '' ?>">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="streetnumber">Street number:</label>
+                        <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?= isset($_POST['streetnumber']) ? htmlspecialchars($_POST['streetnumber']) : '' ?>">
+                    </div>
                 </div>
-                <div class="form-group col-md-6">
-                    <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control">
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="city">City:</label>
+                        <input type="text" id="city" name="city" class="form-control" value="<?= isset($_POST['city']) ? htmlspecialchars($_POST['city']) : '' ?>">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="zipcode">Zipcode</label>
+                        <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?= isset($_POST['zipcode']) ? htmlspecialchars($_POST['zipcode']) : '' ?>">
+                    </div>
                 </div>
-            </div>
-        </fieldset>
+            </fieldset>
 
-        <fieldset>
-            <legend>Products</legend>
-            <?php foreach ($products as $i => $product): ?>
-                <label>
-					<?php // <?= is equal to <?php echo ?>
-                    <input type="checkbox" value=<?= $i ?> name="products[<?= $i ?>]"/> <?= $product['name'] ?> -
-                    &euro; <?= number_format($product['price'], 2) ?></label><br />
-            <?php endforeach; ?>
-        </fieldset>
+            <fieldset>
+                <legend>Products</legend>
+                <?php foreach ($products as $i => $product) : ?>
+                    <label>
+                        <input type="checkbox" value="<?= $i ?>" name="products[<?= $i ?>]" <?= in_array($i, $_POST['products'] ?? []) ? 'checked' : '' ?> />
+                        <?= $product['name'] ?> - &euro; <?= number_format($product['price'], 2) ?>
+                    </label><br />
+                <?php endforeach; ?>
+            </fieldset>
 
-        <button type="submit" class="btn btn-primary">Order!</button>
-    </form>
+            <button type="submit" class="btn btn-primary">Order!</button>
+        </form>
 
-    <footer>You already ordered <strong>&euro; <?= $totalValue ?></strong> in food and drinks.</footer>
-</div>
+        <footer>You already ordered <strong>&euro; <?= $totalValue ?></strong> in food and drinks.</footer>
+    </div>
 
-<style>
-    footer {
-        text-align: center;
-    }
-</style>
+    <style>
+        footer {
+            text-align: center;
+        }
+    </style>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -11,14 +11,20 @@ session_start();
 
 //Variables
 $email = "";
-$adress = ['street' => "",
-'streetnumber' => "",
-'city' => "",
-'zipcode' => ""];
+$adress = [
+    'street' => "",
+    'streetnumber' => "",
+    'city' => "",
+    'zipcode' => ""
+];
 $cart = [];
 
+$invalidFields = [];
+
+
 // Use this function when you need to need an overview of these variables
-function whatIsHappening() {
+function whatIsHappening()
+{
     echo '<h2>$_GET</h2>';
     var_dump($_GET);
     echo '<h2>$_POST</h2>';
@@ -33,18 +39,17 @@ whatIsHappening();
 
 // TODO: provide some products (you may overwrite the example)
 $products = [
-['name' => 'Frosted Flakes', 'price' => 3.99],
-['name' => 'Cheerios', 'price' => 2.89],
-['name' => 'Cocoa Puffs', 'price' => 4.25],
-['name' => 'Honey Nut Cheerios', 'price' => 3.49],
-['name' => 'Lucky Charms', 'price' => 4.75],
-['name' => 'Cinnamon Toast Crunch', 'price' => 3.99],
-['name' => 'Raisin Bran', 'price' => 3.29],
-['name' => 'Cap\'n Crunch', 'price' => 3.95],
-['name' => 'Special K', 'price' => 3.49],
-['name' => 'Granola Clusters', 'price' => 4.99]
-]
-;
+    ['name' => 'Frosted Flakes', 'price' => 3.99],
+    ['name' => 'Cheerios', 'price' => 2.89],
+    ['name' => 'Cocoa Puffs', 'price' => 4.25],
+    ['name' => 'Honey Nut Cheerios', 'price' => 3.49],
+    ['name' => 'Lucky Charms', 'price' => 4.75],
+    ['name' => 'Cinnamon Toast Crunch', 'price' => 3.99],
+    ['name' => 'Raisin Bran', 'price' => 3.29],
+    ['name' => 'Cap\'n Crunch', 'price' => 3.95],
+    ['name' => 'Special K', 'price' => 3.49],
+    ['name' => 'Granola Clusters', 'price' => 4.99]
+];
 
 $totalValue = 0;
 
@@ -52,7 +57,19 @@ function validate()
 {
     // TODO: This function will send a list of invalid fields back
 
-    return [];
+    $fields = ['email', 'street', 'streetnumber', 'city', 'zipcode', 'products'];
+    foreach ($fields as $field) {
+        $value = $_POST[$field];
+        if (empty($value)) {
+            $invalidFields[] = $field;
+        }
+        if ($field === 'zipcode') {
+            if (!is_numeric($value)) {
+                $invalidFields[] = $field;
+            }
+        }
+    }
+    return $invalidFields;
 }
 
 
@@ -62,25 +79,29 @@ function handleForm()
     global $cart, $products;
 
     // TODO: form related tasks (step 1)
-    $email = $_POST['email'];
-    $adress = ['street' => $_POST['street'],
-            'streetnumber' => $_POST['streetnumber'],
-            'city' => $_POST['city'],
-            'zipcode' => $_POST['zipcode']];
-    foreach ($_POST['products'] as $index){
-        if(!empty($products[$index])){
+    $email = htmlspecialchars($_POST['email']);
+    $adress = [
+        'street' => htmlspecialchars($_POST['street']),
+        'streetnumber' => htmlspecialchars($_POST['streetnumber']),
+        'city' => htmlspecialchars($_POST['city']),
+        'zipcode' => htmlspecialchars($_POST['zipcode'])
+    ];
+    foreach ($_POST['products'] as $index) {
+        if (!empty($products[$index])) {
             $cart[] = $products[$index];
         }
     }
-    
-    echo '<pre>'; print_r($cart); echo '</pre>';
 
     // Validation (step 2)
     $invalidFields = validate();
     if (!empty($invalidFields)) {
         // TODO: handle errors
+        foreach ($invalidFields as $invalidField) {
+            echo 'Please enter a valid ' . $invalidField;
+        }
     } else {
         // TODO: handle successful submission
+        echo 'Your order has been submitted.';
     }
 }
 
@@ -90,8 +111,8 @@ if ($formSubmitted) {
     handleForm();
 }*/
 
-  if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        handleForm();
-    }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    handleForm();
+}
 
 require 'form-view.php';
